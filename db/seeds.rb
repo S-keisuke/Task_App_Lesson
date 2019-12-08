0dd1b60998da4d55cf2c7ff6119b517abe31247d
EC2 Default User <ec2-user@ip-172-31-42-50.ap-northeast-1.compute.internal>
2019-12-08 02:16:47 +0000

    ユーザー詳細画面終了

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
 # coding: utf-8
 
+User.create!(name:  "管理者",
+             email: "admin@example.jp",
+             password:  "password",
+             password_confirmation: "password",
+             admin: true)
+             
 User.create!( name: "Sample User",
               email: "sample@email.com",
               password: "password",
               password_confirmation: "password")
+              
